127d1ea90e1d232759cdcc27651f1af5321ce14a
Daniel Maia <daniel.maia.sousa@gmail.com>
2025-11-23 15:51:56 -0300

    Venv1

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,7 +1,6 @@
 import sqlite3
 import psycopg2
 # path / url de conexão
-DB_PATH = "postgresql://neondb_owner:npg_5utNwhLvEel4@ep-lively-mud-acbp5jaw-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require"
-
+DB_PATH ="postgresql://neondb_owner:npg_iBuW8wRrOqf2@ep-calm-mountain-ahjif78i-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require"
 def get_connection():
     return psycopg2.connect(DB_PATH)
